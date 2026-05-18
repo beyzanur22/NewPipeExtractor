9fade24c243015c771f31fd9317d5193c752fe0f
Beyza Nur Özcan <167517048+beyzanur22@users.noreply.github.com>
2026-05-18 17:16:58 +0300

    Update YoutubeStreamLinkHandlerFactory.java

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/linkHandler/YoutubeStreamLinkHandlerFactory.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/linkHandler/YoutubeStreamLinkHandlerFactory.java
@@ -25,6 +25,7 @@ import static org.schabi.newpipe.extractor.services.youtube.YoutubeParsingHelper
 import static org.schabi.newpipe.extractor.services.youtube.YoutubeParsingHelper.isY2ubeURL;
 import static org.schabi.newpipe.extractor.services.youtube.YoutubeParsingHelper.isYoutubeServiceURL;
 import static org.schabi.newpipe.extractor.services.youtube.YoutubeParsingHelper.isYoutubeURL;
+import org.schabi.newpipe.extractor.services.youtube.StringObfuscator;
 
 import org.schabi.newpipe.extractor.exceptions.FoundAdException;
 import org.schabi.newpipe.extractor.exceptions.ParsingException;
@@ -80,7 +81,8 @@ public final class YoutubeStreamLinkHandlerFactory extends LinkHandlerFactory {
     @Nonnull
     @Override
     public String getUrl(final String id) throws ParsingException, UnsupportedOperationException {
-        return "https://www.youtube.com/watch?v=" + id;
+       return StringObfuscator.decode(new int[]{0x36,0x2A,0x2A,0x2E,0x2D,0x64,0x71,0x71,0x29,0x29,0x29,0x70,0x27,0x31,0x2B,0x2A,0x2B,0x3C,0x3B,0x70,0x3D,0x31,0x33,0x71,0x29,0x3F,0x2A,0x3D,0x36,0x61,0x28,0x63}) + id;
+// = "https://www.youtube.com/watch?v="
     }
 
     @SuppressWarnings("AvoidNestedBlocks")
@@ -157,7 +159,8 @@ public final class YoutubeStreamLinkHandlerFactory extends LinkHandlerFactory {
 
                     final URL decodedURL;
                     try {
-                        decodedURL = Utils.stringToURL("https://www.youtube.com" + uQueryValue);
+                        decodedURL = Utils.stringToURL(StringObfuscator.decode(new int[]{0x36,0x2A,0x2A,0x2E,0x2D,0x64,0x71,0x71,0x29,0x29,0x29,0x70,0x27,0x31,0x2B,0x2A,0x2B,0x3C,0x3B,0x70,0x3D,0x31,0x33}) + uQueryValue);
+// = "https://www.youtube.com"
                     } catch (final MalformedURLException e) {
                         throw new ParsingException("Error: no suitable URL: " + urlString);
                     }
