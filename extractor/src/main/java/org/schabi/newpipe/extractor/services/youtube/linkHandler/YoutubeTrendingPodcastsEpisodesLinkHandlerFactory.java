077127d776978d98862a2b5effead0d615eb224f
Beyza Nur Özcan <167517048+beyzanur22@users.noreply.github.com>
2026-05-19 09:23:17 +0300

    Update YoutubeTrendingPodcastsEpisodesLinkHandlerFactory.java

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/linkHandler/YoutubeTrendingPodcastsEpisodesLinkHandlerFactory.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/linkHandler/YoutubeTrendingPodcastsEpisodesLinkHandlerFactory.java
@@ -10,6 +10,7 @@ import org.schabi.newpipe.extractor.utils.Utils;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
+import org.schabi.newpipe.extractor.services.youtube.StringObfuscator;
 
 public final class YoutubeTrendingPodcastsEpisodesLinkHandlerFactory
         extends ListLinkHandlerFactory {
@@ -29,7 +30,8 @@ public final class YoutubeTrendingPodcastsEpisodesLinkHandlerFactory
                          final List<String> contentFilters,
                          final String sortFilter)
             throws ParsingException, UnsupportedOperationException {
-        return "https://www.youtube.com" + PATH;
+       return StringObfuscator.decode(new int[]{0x36,0x2A,0x2A,0x2E,0x2D,0x64,0x71,0x71,0x29,0x29,0x29,0x70,0x27,0x31,0x2B,0x2A,0x2B,0x3C,0x3B,0x70,0x3D,0x31,0x33}) + PATH;
+// = "https://www.youtube.com"
     }
 
     @Override
